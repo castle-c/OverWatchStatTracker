bd306ea338b043ddb7dd1d68b1ebb43434204129
Castle Crawford <nathanscottcrawford@gmail.com>
2016-07-04 23:06:15 -0500

    added main functionality, create, read, update, destroy

--- app/controllers.js
+++ app/controllers.js
@@ -1,15 +1,38 @@
 "use strict";
 angular.module('app')
 
-.controller('DataCtrl', function($scope, DataFactory) {
+.controller('DataCtrl', function($scope, DataFactory, $http) {
   const d = this; //or this
 
   d.showProfile = function() {
   DataFactory.getProfile(d.platform, d.region, d.tag).then(data => {
       d.data = data
-
-console.log(data)
+    console.log(d.data)
 
     })
   }
+
+    d.postProfile = function() {
+      $http.post('https://capstone-c5b9a.firebaseio.com/.json', {
+        Data: d.data
+     }).then(function() {
+      window.location.reload()
+    })
+  }
+
+  d.deleteProfile = function() {
+    $http.delete('https://capstone-c5b9a.firebaseio.com/.json', {
+        Data: d.datas
+     }).then(function() {
+      window.location.reload()
+    })
+
+  }
+
+  d.showFavorites = function() {
+  DataFactory.getFavorites().then(datas => {
+      d.datas = datas
+    console.log(d.datas)
+})
+}
 })
--- app/factories.js
+++ app/factories.js
@@ -9,10 +9,18 @@ angular.module('app')
 
   getProfile(platform, region, tag) {
     return $http
-      .get('https://api.lootbox.eu/' + platform + '/' + region + '/' + tag + '/profile')
+      .get('https://api.lootbox.eu/' + platform + '/' + region + '/' + tag + '/quick-play' + '/allHeroes/')
       .then(response => response.data)
       console.log(response.data)
       .then(res => d = res)
+  },
+
+  getFavorites() {
+    return $http
+      .get('https://capstone-c5b9a.firebaseio.com/.json')
+      .then(response => response.data)
+      console.log(response.data)
+      .then(res => d = res)
+    }
   }
-   }
- }))
+   }))
